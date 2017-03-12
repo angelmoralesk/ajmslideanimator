10d6580615791f20daeca5d6a6fdbece3e8d7cea
Angel Morales Karam <angelmorales@deloittemx.com>
2017-03-12 15:17:17 -0600

    adding images and gitignore file

---
+++ AJMSlideAnimator/Views/AJMSlideAnimatorView.swift
@@ -0,0 +1,9 @@
+//
+//  AJMSlideAnimatorView.swift
+//  AJMSlideAnimator
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 12/03/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import Foundation
