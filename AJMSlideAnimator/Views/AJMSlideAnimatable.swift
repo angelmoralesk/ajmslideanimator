a648aadc6206447a2acd6a71c5c303f8120158f4
Angel Morales Karam <a.jesse3001@gmail.com>
2017-08-01 22:51:35 -0500

    create a protocol that will compromise a family of animations algorithms

---
+++ AJMSlideAnimator/Views/AJMSlideAnimatable.swift
@@ -0,0 +1,9 @@
+//
+//  AJMSlideAnimatable.swift
+//  AJMSlideAnimator
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 01/08/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import Foundation
