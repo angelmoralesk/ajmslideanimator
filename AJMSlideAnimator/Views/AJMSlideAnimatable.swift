e2e922dbd078dd2b5f9d7e39274bf716b7f40d9f
Angel Morales Karam <a.jesse3001@gmail.com>
2017-08-02 10:14:44 -0500

    include two properties  and initialize them in the init

--- AJMSlideAnimator/Views/AJMSlideAnimatable.swift
+++ AJMSlideAnimator/Views/AJMSlideAnimatable.swift
@@ -20,12 +20,25 @@ struct SingleVerticalAnimation : AJMAnimatable {
     
     let rect : CGRect
     let mainImageView : UIImageView
+    var compOne : UIImageView
+    var compTwo : UIImageView
+    
     let style : AJMSlideAnimatorStyle
     
     init(rect : CGRect, imageView :UIImageView, style :AJMSlideAnimatorStyle) {
         self.rect = rect
         self.mainImageView = imageView
         self.style = style
+     
+        let width = CGFloat(rect.width)/2
+        let height = CGFloat(rect.height)
+        
+        let firstRect = CGRect(x: 0, y: 0, width: width, height: height)
+        compOne = UIImageView(frame: firstRect)
+        
+        let secondRect = CGRect(x: width, y: 0, width: width, height: height)
+        compTwo = UIImageView(frame: secondRect)
+        
     }
     
     func prepareImageViews() {
