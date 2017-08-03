30ef091a19629ce5eaa2fc5f3357ed60fc5ddb49
Angel Morales Karam <a.jesse3001@gmail.com>
2017-08-03 17:16:34 -0500

    adding a new struct MultipleVerticalAnimation

---
+++ AJMSlideAnimator/Views/MultipleVerticalAnimation.swift
@@ -0,0 +1,9 @@
+//
+//  MultipleVerticalAnimation.swift
+//  AJMSlideAnimator
+//
+//  Created by Angel Jesse Morales Karam Kairuz on 03/08/17.
+//  Copyright © 2017 TheKairuzBlog. All rights reserved.
+//
+
+import Foundation
