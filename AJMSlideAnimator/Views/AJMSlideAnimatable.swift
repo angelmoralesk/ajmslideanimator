3395588ba9aacf26f4b67b50747a5c19b7617019
Angel Morales Karam <a.jesse3001@gmail.com>
2017-09-01 13:05:47 -0500

    updating protocol to use new API functions for animations

--- AJMSlideAnimator/Views/AJMSlideAnimatable.swift
+++ AJMSlideAnimator/Views/AJMSlideAnimatable.swift
@@ -12,4 +12,20 @@ protocol AJMAnimatable {
     
     func prepareContent()
     func animate(completion : @escaping (Bool) -> ())
+    
+    // NEW API FOR IOS 10 WITH PROPERTY ANIMATOR    
+    func animateOnUserInteractionContent(completion : ((Bool) -> ())?) // when user taps, the animation  should reverse
+    func pause()
+    func updateProgress(progress : Double)
+    func resume()
+}
+
+extension AJMAnimatable {
+    
+    func animateOnUserInteractionContent(completion : ((Bool) -> ())?){ }
+    func pause() {}
+    func updateProgress(progress : Double) {}
+    func resume() {}
+
+
 }
